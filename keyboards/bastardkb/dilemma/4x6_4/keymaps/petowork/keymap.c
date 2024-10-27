cb0e2aa2b01474a92ee661fb4edb662f76a0398f
peto <j.r.klement@proton.me>
2024-10-27 16:51:18 +0000

    Revert "tamed RGB"
    This reverts commit 078bd2c8e41ab1f8f510fa34807abab7cfb56ecf.

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/petowork/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/petowork/keymap.c
@@ -115,59 +115,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #endif     // POINTING_DEVICE_ENABLEE
 
 #ifdef RGB_MATRIX_ENABLE
-static HSV _get_hsv_for_layer_index(uint8_t layer) {
-    switch (layer) {
-        case 0:
-            return (HSV){HSV_BLACK};
-        case 1:
-            return (HSV){HSV_BLUE};
-        case 2:
-            return (HSV){HSV_RED};
-        case 3:
-            return (HSV){HSV_CHARTREUSE};
-        case 4:
-            return (HSV){HSV_GREEN};
-        case 5:
-            return (HSV){HSV_TEAL};
-        case 6:
-            return (HSV){HSV_PURPLE};
-        default:
-            return (HSV){HSV_ORANGE};
-    };
-}
-
-// Layer state indicator
-bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
-        return false;
-    }
-    // Set indicator LEDs to red if caps lock is enabled
-    if (host_keyboard_led_state().caps_lock) {
-        for (int i = led_min; i <= led_max; i++) {
-            if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) {
-                // set modifier-flagged LEDs to a pure a configured interval brighter than the current brightness, clamped to 255 (ie. uint8_t max value).
-                rgb_matrix_set_color(i, MIN(rgb_matrix_get_val() + CAPSLOCK_INDICATOR_BRIGHTNESS_INC, 255), 0, 0);
-            }
-        }
-    }
-
-    // Set underglow to a solid color for highest active layer apart from the base layer.
-    const uint8_t layer = get_highest_layer(layer_state);
-    if (layer > -1) {
-        HSV hsv = _get_hsv_for_layer_index(layer);
-
-        // Set brightness to the configured interval brighter than current brightness, clamped to 255 (ie. uint8_t max value). This compensates for the dimmer appearance of the underglow LEDs.
-        hsv.v         = MIN(rgb_matrix_get_val() + LAYER_INDICATOR_BRIGHTNESS_INC, 255);
-        const RGB rgb = hsv_to_rgb(hsv);
-
-        for (int i = led_min; i < led_max; i++) {
-            if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
-                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-            }
-        }
-    }
-    return true;
-};
+// Forward-declare this helper function since it is defined in rgb_matrix.c.
+void rgb_matrix_update_pwm_buffers(void);
 #endif // RGB_MATRIX_ENABLE
 
 #ifdef ENCODER_MAP_ENABLE
