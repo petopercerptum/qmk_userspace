d7e52d6fd1bffbf13bb9a8c290717b503b3748d2
peto <j.r.klement@proton.me>
2024-10-27 16:58:29 +0000

    tamed RGB

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/petowork/config.h
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/petowork/config.h
@@ -25,3 +25,8 @@
 #define AUTO_SHIFT_TIMEOUT 175
 #define AUTO_SHIFT_ENTER
 #define RETRO_SHIFT 500
+// rgb
+#define ENABLE_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 0 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL 128 // Sets the default brightness value, if none has been set
